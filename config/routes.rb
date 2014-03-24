5a135d4d86e62cc3b89132650c90d9ee499876f3
son <son@lincolnix.net>
2014-03-24 22:36:05 +0000

    Implement pins resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Pinteresting::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
   get "contact" => "pages#contact" 
   get "signup" => "pages#signup"
   get "login" => "pages#login"
+  get "pins" => "pins#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
