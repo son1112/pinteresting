c8a5897d1938413ce1f67b6a9fc1d383bccc437b
son <son@lincolnix.net>
2014-04-13 19:24:52 +0000

    remove confirmed_at, add it back, then update all users to current time for confirmed_at, in attempt to update old users on heroku production deployment

---
+++ db/migrate/20140413192213_add_confirmable_to_users.rb~
@@ -0,0 +1,4 @@
+class AddConfirmableToUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413073920) do
+ActiveRecord::Schema.define(version: 20140413192213) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -38,12 +38,12 @@ ActiveRecord::Schema.define(version: 20140413073920) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "confirmation_token"
-    t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.datetime "confirmed_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
